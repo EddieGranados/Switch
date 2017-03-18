3629cda2c43d9596865296a2f23623069f2cad67
Eddie Granados <egranados04@live.com>
2017-03-18 13:31:29 -0400

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,22 @@
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+		char penaltyKick = 'a';
+		
+		switch(penaltyKick) {
+		
+		case 'L' : System.out.println("Hello World!");
+			break;
+		case 'R': System.out.println("Messi shoots to the right and misses the goal!");
+			break;
+		case 'C': System.out.println("Messi shoots down the center, but the keeper blocks it!");
+			break;
+		default:
+			System.out.println("Messi is in position...");
+		}
+
+	}
+
+}
